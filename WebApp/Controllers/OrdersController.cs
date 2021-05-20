d008b4df75075ed8710c25920b6d2dd9dda907f9
evgiv0 <zhe956@gmail.com>
2021-05-20 21:16:16 +0300

    work with useCases

--- WebApp/Controllers/OrdersController.cs
+++ WebApp/Controllers/OrdersController.cs
@@ -1,6 +1,9 @@
-﻿using Application;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using UseCases;
+using UseCases.Order.Commands.CreateOrder;
+using UseCases.Order.Queries.GetOrderById;
 
 namespace WebApp.Controllers
 {
@@ -8,17 +11,23 @@ namespace WebApp.Controllers
     [Route("[controller]")]
     public class OrdersController : ControllerBase
     {
-        private readonly IOrderService _orderService;
+        private readonly ISender _sender;
 
-        public OrdersController(IOrderService orderService)
+        public OrdersController(ISender sender)
         {
-            _orderService = orderService;
+            _sender = sender;
         }
 
         [HttpGet("{id}")]
         public async Task<OrderDto> Get(int id)
         {
-            return await _orderService.GetByIdAsync(id);
+            return await _sender.Send(new GetOrderByIdQuery { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<int> Create([FromBody] CreateOrderDto order)
+        {
+            return await _sender.Send(new CreateOrderCommand { Order = order });
         }
 
     }
